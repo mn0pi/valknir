a2250905b91f864075087e178197098f5bfa7887
mn0pi <nueva.emenopi@proton.me>
2026-04-04 16:14:03 +0100

    Implement basic cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+mod cli;
 
+use clap::Parser;
+use cli::{Cli, Commands};
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Analyse { file } => {
+            println!("[valknir] analysing: {}", file);
+        }
+    };
 }
